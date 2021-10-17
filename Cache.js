6aba2cb1f44bc8c69b67fb23d4d57b33f9356227
Swapnil <90128544+SwaPra69@users.noreply.github.com>
2021-10-17 08:15:18 +0530

    Add files via upload

---
+++ Cache.js
@@ -0,0 +1,15 @@
+//Version: 1
+
+self.addEventListener('install', (e) => {
+e.waitUntil(
+caches.open('Speedy').then((cache) => cache.addAll([
+'Speedy.css',
+])),
+);
+});
+
+self.addEventListener('fetch', (e) => {
+e.respondWith(
+caches.match(e.request).then((response) => response || fetch(e.request)),
+);
+});
