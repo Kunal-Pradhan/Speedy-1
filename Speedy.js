02bfc65177e85c22cae079221efc1fdb8fce30fd
Swapnil <90128544+Swapnil-Pradhan@users.noreply.github.com>
2022-01-01 20:00:10 +0530

    Update Speedy.js

--- Speedy.js
+++ Speedy.js
@@ -1,3 +1,10 @@
+let deferredPrompt;
+window.addEventListener('beforeinstallprompt', (e) => {
+e.preventDefault();
+deferredPrompt = e;
+showInstallPromotion();
+});
+
 if('serviceWorker' in navigator){
 alert("done");
 navigator.serviceWorker.register('SpeedyWorker.js');
