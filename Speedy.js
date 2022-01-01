a260d259d0cdb887d941595a407277acc44f14e6
Swapnil <90128544+Swapnil-Pradhan@users.noreply.github.com>
2022-01-01 14:57:58 +0530

    Update Speedy.js

--- Speedy.js
+++ Speedy.js
@@ -1,5 +1,11 @@
-if("serviceWorker" in navigator){
-navigator.serviceWorker.register("SpeedyWorker.js");
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('SpeedyWorker.js')
+  .then(function(registration) {
+    alert('Registration successful, scope is:', registration.scope);
+  })
+  .catch(function(error) {
+    alert('Service worker registration failed, error:', error);
+  });
 }
 
 let deferredPrompt;
